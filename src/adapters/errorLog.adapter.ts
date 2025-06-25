b5f00ca31eacd5a91f14092e2abd7e8c1e437248
faithfulcoronel <39255507+faithfulcoronel@users.noreply.github.com>
2025-06-25 17:48:56 +0800

    Add ErrorLogs entity and service

---
+++ src/adapters/errorLog.adapter.ts
@@ -0,0 +1,24 @@
+import 'reflect-metadata';
+import { injectable } from 'inversify';
+import { BaseAdapter } from './base.adapter';
+import { ErrorLog } from '../models/errorLog.model';
+
+export interface IErrorLogAdapter extends BaseAdapter<ErrorLog> {}
+
+@injectable()
+export class ErrorLogAdapter
+  extends BaseAdapter<ErrorLog>
+  implements IErrorLogAdapter
+{
+  protected tableName = 'error_logs';
+
+  protected defaultSelect = `
+    id,
+    message,
+    stack,
+    context,
+    created_at,
+    tenant_id,
+    created_by
+  `;
+}
