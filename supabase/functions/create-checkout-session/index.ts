ae3b79567c659ac5a170891732e6824ceec3bea3
faithfulcoronel <39255507+faithfulcoronel@users.noreply.github.com>
2025-07-03 00:26:26 +0800

    fix: allow custom header for supabase functions

--- supabase/functions/create-checkout-session/index.ts
+++ supabase/functions/create-checkout-session/index.ts
@@ -4,7 +4,8 @@ import Stripe from 'https://esm.sh/stripe@13.10.0';
 
 const corsHeaders = {
   'Access-Control-Allow-Origin': '*',
-  'Access-Control-Allow-Headers': 'authorization, x-client-info, apikey, content-type',
+  'Access-Control-Allow-Headers':
+    'authorization, x-client-info, apikey, content-type, x-application-name',
 };
 
 interface RequestBody {
