490bd477d33ac39378200236610c34c7cd5bd0be
faithfulcoronel <39255507+faithfulcoronel@users.noreply.github.com>
2025-07-03 23:43:01 +0800

    fix: limit table overflow to x-axis

--- src/components/ui2/table.tsx
+++ src/components/ui2/table.tsx
@@ -5,7 +5,7 @@ import { cn } from '@/lib/utils';
 
 const Table = React.forwardRef<HTMLTableElement, React.HTMLAttributes<HTMLTableElement>>(
   ({ className, ...props }, ref) => (
-    <div className="relative w-full overflow-auto">
+    <div className="relative w-full overflow-x-auto">
       <table ref={ref} className={cn('w-full caption-bottom text-sm dark:text-foreground', className)} {...props} />
     </div>
   )
