009d4e6dcdbe48bcd0e11fc3159778422335bab8
faithfulcoronel <39255507+faithfulcoronel@users.noreply.github.com>
2025-07-03 17:31:44 +0800

    Add gradient background to sidebar

--- src/components/layout/Sidebar.tsx
+++ src/components/layout/Sidebar.tsx
@@ -302,7 +302,7 @@ function Sidebar({
       {/* Sidebar */}
       <aside
         className={`
-          fixed inset-y-0 left-0 z-50 bg-gray-900 flex flex-col
+          fixed inset-y-0 left-0 z-50 bg-gray-900 bg-primary-gradient flex flex-col
           transform transition-transform duration-300 ease-in-out
           ${sidebarOpen ? 'translate-x-0' : '-translate-x-full'}
           lg:translate-x-0
